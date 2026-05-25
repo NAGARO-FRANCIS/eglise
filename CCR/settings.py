a4c5dc05f84486d78e33dc47b80b8595be374be8
NAGARO-FRANCIS <nagarofrancis697@gmail.com>
2026-05-25 23:17:57 +0000

    fix session engine

--- CCR/settings.py
+++ CCR/settings.py
@@ -150,6 +150,4 @@ CACHES = {
         }
     }
 }
-
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-SESSION_CACHE_ALIAS = 'default'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
